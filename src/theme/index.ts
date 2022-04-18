3d12342a4403d629cfe2a404db4dcfb1850fa0e2
Marek Miklaszewski <mar.mik1@wp.pl>
2022-04-18 10:49:36 +0200

    chore: add defaultColorScheme for Buttons

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,11 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
 
-const theme = extendTheme({
+const defaultColorSchemeComponents = withDefaultColorScheme({
+  colorScheme: "purple",
+  components: ["Button"],
+});
+
+const theme = extendTheme(defaultColorSchemeComponents, {
   fonts: {
     body: "Poppins, sans-serif",
   },
